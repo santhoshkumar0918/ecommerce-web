63106fc7bc6fd45480d0603eee4939acac5c0def
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-12 16:38:27 +0530

    added

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <div className="bg-blue-400">hello 1233</div>
+    <Button className="">Click me</Button>
+    </>
   );
 }
