2e0ec2547f654ec8a5bd79b933f3700c9b17b780
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-14 21:15:35 +0530

    added

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -5,11 +5,9 @@ export const structure: StructureResolver = (S) =>
   S.list()
     .title('Shopere Ecom Web')
     .items([
-      S.documentTypeListItem('post').title('Posts'),
       S.documentTypeListItem('category').title('Categories'),
-      S.documentTypeListItem('author').title('Authors'),
       S.divider(),
       ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
+        (item) => item.getId() && !['post', 'category'].includes(item.getId()!),
       ),
     ])
