5b563dcc372b3dbc0069b92a5bc0528847dcf423
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-14 19:37:17 +0530

    added

---
+++ sanity/schemaTypes/productType.tsx
@@ -0,0 +1,10 @@
+import {  TrolleyIcon } from "@sanity/icons";
+
+
+export default {
+    name: "productType",
+    title: "productType",
+    type: "document",
+    icon: TrolleyIcon,
+    fields : []
+}
