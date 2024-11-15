18d83c26add7d2a829abc958661854569bbd7fa4
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 13:28:37 +0530

    added

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,11 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation,
+  stega: {
+    studioUrl: process.env.VERCEL_URL 
+      ? `https://${process.env.VERCEL_URL}/Studio`
+      : `${process.env.NEXT_PUBLIC_BASE_URL}/Studio`,
+      
+  }
 })
