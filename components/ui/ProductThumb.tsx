f61f9ea5c68b1251def62f94b381b2331573c4b8
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 21:19:23 +0530

    added

--- components/ui/ProductThumb.tsx
+++ components/ui/ProductThumb.tsx
@@ -1,11 +1,17 @@
 "use client"
 import { Product } from "@/sanity.types"
-import React from "react"
+import Link from "next/link";
 
 function ProductThumb({ product}: { product: Product }) {
-    const isOutOfStock = product.stock !== null && (product.stock??0 )<= 0;
+    const isOutOfStock = product.stock != null && product.stock <= 0;
     return (
-<></>
+        <Link
+            href={`/product/${product.slug?.current}`}
+            className={`group flex flex-col bg-white rounded-lg border border-gray-200 shadow-sm hover:shadow-sm transition-all duration-200 overflow-hidden
+            ${isOutOfStock ? "opacity-50" : ""}`}>
+            Product
+            
+        </Link>
     )   
 }
 
