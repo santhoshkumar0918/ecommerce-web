9fd3882f94dcc69d1c813ad7df14b1b75881b153
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 12:36:23 +0530

    added

--- sanity/schemaTypes/salesType.ts
+++ sanity/schemaTypes/salesType.ts
@@ -1,31 +1,58 @@
+// import { TagIcon } from "@sanity/icons";
+// import { defineType } from "sanity";
+
+// export const salesType = defineType({
+//     name: "sale",
+//     title: "Sale",
+//     type: "document",
+//     icon: TagIcon,
+//     fields: [
+//         defineType({
+//             name: "title",
+//             type:"string",
+//             title: "Sale Title",
+           
+//         }),
+//           defineType({
+//             name: "description",
+//             type:"text",
+//             title: "Sale Description",
+//           }),
+//            defineType({
+//             name: "discountAmount",
+//             type:"number",
+//                title: "Discount Amount",
+//             description:"Amount off in percentage or  fixed values"
+//         }),
+
+
+//     ]
+
+// })
 import { TagIcon } from "@sanity/icons";
 import { defineType } from "sanity";
 
 export const salesType = defineType({
-    name: "sale",
-    title: "Sale",
-    type: "document",
-    icon: TagIcon,
-    fields: [
-        defineType({
-            name: "title",
-            type:"string",
-            title: "Sale Title",
-           
-        }),
-          defineType({
-            name: "description",
-            type:"text",
-            title: "Sale Description",
-          }),
-           defineType({
-            name: "discountAmount",
-            type:"number",
-               title: "Discount Amount",
-            description:"Amount off in percentage or  fixed values"
-        }),
-
-
-    ]
-
-})
+  name: "sale",
+  title: "Sale",
+  type: "document",
+  icon: TagIcon,
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Sale Title",
+    },
+    {
+      name: "description",
+      type: "text",
+      title: "Sale Description",
+    },
+    {
+      name: "discountAmount",
+      type: "number",
+      title: "Discount Amount",
+      description: "Amount off in percentage or fixed values",
+    },
+  ],
+});
