066d8bc9eb98f7feefa429ab470daec52e748c81
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 12:24:51 +0530

    updated

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,10 +2,9 @@ import { type SchemaTypeDefinition } from 'sanity'
 
 import {blockContentType} from './blockContentType'
 import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
 import { productType } from './productType'
+import { orderType } from './orderType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType,productType],
+  types: [blockContentType, categoryType,productType,orderType],
 }
