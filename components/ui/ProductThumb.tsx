aeea747f5e262cb6174d75100c4cbb544bc1e984
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 20:46:51 +0530

    fixed type

--- components/ui/ProductThumb.tsx
+++ components/ui/ProductThumb.tsx
@@ -2,11 +2,11 @@
 import { Product } from "@/sanity.types"
 import React from "react"
 
-function ProductThumb({ product}: { product: Product[] }) {
+function ProductThumb({ product}: { product: Product }) {
     const isOutOfStock = product.stock !== null && product.stock <= 0;
     return (
 <></>
-    )
+    )   
 }
 
 export default ProductThumb
