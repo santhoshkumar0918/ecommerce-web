ccaa2888cf069776089293b3e4741c1d9f21e8e0
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 11:39:11 +0530

    added

--- sanity/schemaTypes/orderType.ts
+++ sanity/schemaTypes/orderType.ts
@@ -1,5 +1,5 @@
 import {  BasketIcon, TrolleyIcon } from "@sanity/icons";
-import {  defineField, defineType } from "sanity";
+import {  defineArrayMember, defineField, defineType } from "sanity";
 
 
 export const orderType = defineType({
@@ -42,7 +42,53 @@ export const orderType = defineType({
             title: "Customer Email",
             type: "string",
             validation: (Rule) => Rule.required().email()
-        }),
+          }),
+           defineField({
+            name: "stripePaymentIntentId",
+            title: "Stripe Payment Intent Id",
+            type: "string",
+            validation: (Rule) => Rule.required()
+           }),
+           defineField({
+            name: "product",
+            title: "Products",
+            type: "array",
+               of: [
+                   defineArrayMember({
+                       type: "object",
+                       fields: [
+                           defineField({
+                               name: "product",
+                               title: 'Product Bougth',
+                               type: "reference",
+                               to: [{ type: "product" }]
+                           }),
+                           defineField({
+                               name: "quantity",
+                               title: 'Quanitity Purchased',
+                               type: "number",
+                           }),
+                       ],
+                       preview: {
+                           select: {
+                               product: "product.name",
+                               quantity: "quantity",
+                               image: "product.image",
+                               price: "product.price",
+                               currency:"product.currency",
+                           },
+                           prepare(select) {
+                               return {
+                                   title: `${select.product} x ${select.quantity},`,
+                                   subtitle: `${select.quantity * select.price}`,
+                                   media:select.image,a
+                               }
+                           }
+                       }
+                })
+            ]
+           }),
+           
         
     ]
 
