5ba6e4d55bdafa88b5e29a0a69aa45b8a3eb70e9
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 18:32:03 +0530

    updated lib

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,16 +1,18 @@
 import { Button } from "@/components/ui/button";
 import ProductsView from "@/components/ui/ProductsView";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 import Image from "next/image";
 
 export default async function Home() {
 
   const products = await getAllProducts();
+  const categories = await getAllCategories();
   return (
     <>
       <div className="bg-blue-400">hello 1233
         
-        <div>
+        <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100">
           <ProductsView products={products}/>
         </div>
     </div>
