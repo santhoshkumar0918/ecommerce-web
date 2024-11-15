9768f79e277f2ab7fe6268b5ae01b3d146d1d16f
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 17:52:52 +0530

    added

--- sanity/lib/products/getAllProducts.ts
+++ sanity/lib/products/getAllProducts.ts
@@ -3,5 +3,20 @@ import { sanityFetch } from "../live";
 
 
 export const getAllProducts = async () => {
-    
+    const ALL_PRODUCTS_QUERY = defineQuery(`
+        *[
+            _type == "product"] 
+          | order(name asc)
+    `)
+
+    try {
+        const products = await sanityFetch({
+            query:ALL_PRODUCTS_QUERY,
+        })
+
+        return products.data || [];
+    } catch (error) {
+        console.error("Error couldn't fetch all the products", error);
+        return [];
+    }
 }
