78d76cd13844f03b03f3fc0b6b52ddf7ec60274d
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 19:57:12 +0530

    updated

--- components/ui/ProductGrid.tsx
+++ components/ui/ProductGrid.tsx
@@ -1,30 +1,29 @@
-import { Product } from '@/sanity.types'
-import React from 'react'
-import {AnimatePresence,motion} from "framer-motion"
 
-function ProductGrid({products} : {products : Product[]}){
+import { Product } from '@/sanity.types';
+import React from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import ProductThumb from './ProductThumb'; 
+
+function ProductGrid({ products }: { products: Product[] }) {
   return (
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 '>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
           {products?.map((product) => {
               return (
-                  <AnimatePresence key={product._id}
-                      
-                  >
+                  <AnimatePresence key={product._id}>
                       <motion.div
-                          layout,
-                      initial={{ opacity: 0.2 }},
-                      animate={{ opacity: 1 }},
-                      exit={{ opacity: 0 }},
-                      className="flex justify-center"
+                          layout
+                          initial={{ opacity: 0.2 }}
+                          animate={{ opacity: 1 }}
+                          exit={{ opacity: 0 }}
+                          className="flex justify-center"
                       >
-                          <ProductThumb key={ product._id} product={product} />
-              </motion.div>
+                          <ProductThumb product={product} />
+                      </motion.div>
                   </AnimatePresence>
               )
           })}
-          
-      </div>
-  )
+    </div>
+  );
 }
 
-export default ProductGrid
+export default ProductGrid;
