00b82a6219e007e2bb66082d6d539eb0b897049a
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 20:13:34 +0530

    added

--- components/ui/ProductThumb.tsx
+++ components/ui/ProductThumb.tsx
@@ -1,12 +1,12 @@
 "use client"
+import { Product } from "@/sanity.types"
+import React from "react"
 
-
-import React from 'react'
-
-function ProductThumb() {
-  return (
-    <div>ProductThumb</div>
-  )
+function ProductThumb({ product}: { product: Product[] }) {
+    const isOutOfStock = product.stock !== null && product.stock <= 0;
+    return (
+<></>
+    )
 }
 
 export default ProductThumb
