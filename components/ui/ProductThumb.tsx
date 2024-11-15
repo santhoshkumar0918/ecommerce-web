6c47c8bf9bbabb4b532ea33ffa9b8e8083fb7aa3
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 21:24:02 +0530

    added

--- components/ui/ProductThumb.tsx
+++ components/ui/ProductThumb.tsx
@@ -7,9 +7,15 @@ function ProductThumb({ product}: { product: Product }) {
     return (
         <Link
             href={`/product/${product.slug?.current}`}
-            className={`group flex flex-col bg-white rounded-lg border border-gray-200 shadow-sm hover:shadow-sm transition-all duration-200 overflow-hidden
+            className={`group flex flex-col p-2  bg-white rounded-lg border border-gray-200 shadow-sm hover:shadow-sm transition-all duration-200 overflow-hidden
             ${isOutOfStock ? "opacity-50" : ""}`}>
-            Product
+            <div>
+                {a && (
+                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                        <span className="text-white font-bold text-xl ">Out of Stock</span>
+                    </div>
+                )}
+            </div>
             
         </Link>
     )   
