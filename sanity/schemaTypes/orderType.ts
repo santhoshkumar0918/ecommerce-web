25141cd33c5f0807fe695e8e261022897cdf4e89
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 11:23:11 +0530

    u

--- sanity/schemaTypes/orderType.ts
+++ sanity/schemaTypes/orderType.ts
@@ -8,12 +8,42 @@ export const orderType = defineType({
     type: "document",
     icon: BasketIcon,
     fields: [
-          defineField({
+        defineField({
             name: "orderNumber",
             title: " Order Number",
             type: "string",
-            validation:(Rule) => Rule.required(),
+            validation: (Rule) => Rule.required(),
         }),
+        defineField({
+            name: "stripeCheckoutSessionId",
+            title: "Stripe Checkout Session Id",
+            type: "string",
+        }),
+        defineField({
+            name: "stripeCustomerId",
+            title: "Stripe Customer Id",
+            type: "string",
+            validation: (Rule) => Rule.required()
+        }),
+        defineField({
+            name: "clerkUserId",
+            title: "Store User Id",
+            type: "string",
+            validation: (Rule) => Rule.required()
+        }),
+        defineField({
+            name: "customerName",
+            title: "Customer Name",
+            type: "string",
+            validation: (Rule) => Rule.required()
+        }),
+          defineField({
+            name: "email",
+            title: "Customer Email",
+            type: "string",
+            validation: (Rule) => Rule.required().email()
+        }),
+        
     ]
 
 })
