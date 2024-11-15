91be5c9fa7c20c930e0d28aaaa975c36a4219275
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-15 12:57:36 +0530

    updated

--- sanity/schemaTypes/salesType.ts
+++ sanity/schemaTypes/salesType.ts
@@ -1,58 +1,70 @@
-// import { TagIcon } from "@sanity/icons";
-// import { defineType } from "sanity";
-
-// export const salesType = defineType({
-//     name: "sale",
-//     title: "Sale",
-//     type: "document",
-//     icon: TagIcon,
-//     fields: [
-//         defineType({
-//             name: "title",
-//             type:"string",
-//             title: "Sale Title",
-           
-//         }),
-//           defineType({
-//             name: "description",
-//             type:"text",
-//             title: "Sale Description",
-//           }),
-//            defineType({
-//             name: "discountAmount",
-//             type:"number",
-//                title: "Discount Amount",
-//             description:"Amount off in percentage or  fixed values"
-//         }),
-
-
-//     ]
-
-// })
 import { TagIcon } from "@sanity/icons";
-import { defineType } from "sanity";
+import { defineField, defineType } from "sanity";
 
 export const salesType = defineType({
-  name: "sale",
-  title: "Sale",
-  type: "document",
-  icon: TagIcon,
-  fields: [
-    {
-      name: "title",
-      type: "string",
-      title: "Sale Title",
-    },
-    {
-      name: "description",
-      type: "text",
-      title: "Sale Description",
-    },
-    {
-      name: "discountAmount",
-      type: "number",
-      title: "Discount Amount",
-      description: "Amount off in percentage or fixed values",
-    },
-  ],
-});
+    name: "sale",
+    title: "Sale",
+    type: "document",
+    icon: TagIcon,
+    fields: [
+        defineField({
+            name: "title",
+            type:"string",
+            title: "Sale Title",
+        }),
+        defineField({
+            name: "description",
+            type:"text",
+            title: "Sale Description",
+        }),
+        defineField({
+            name: "discountAmount",
+            title: "Discount Amount",
+            type:"number",
+            description:"Amount off in percentage or  fixed values"
+        }),
+        defineField({
+            name: "couponCode",
+            title: "Coupon Code",
+            type:"string",
+        }),
+        defineField({
+            name: "validForm",
+            title: "Valid Form",
+            type:"datetime",
+        }),
+        defineField({
+            name: "validUntil",
+            title: "Valid Until",
+            type:"datetime",
+        }),
+        defineField({
+            name: "isActive",
+            title: "Is Active",
+            type: "boolean",
+            description: "Toggle to activate/deactive the sale",
+            initialValue:true
+        })
+
+
+    ],
+
+    preview:{
+        select: {
+            title: "title",
+            couponCode: "couponCode",
+            discountAmount: "discountAmount",
+            isActive:"isActive",
+        },
+        prepare(select) {
+            const { title, discountAmount, couponCode, isActive } = select;
+            const status = isActive ? "Active" : "Inactive";
+            return {
+                title,
+                subtitle: `${discountAmount}% off - Code:${couponCode} - ${status}`,
+                
+            };
+        }
+}
+
+})
